a7fdf9cec4cb2c50f7855afb518f44d1cf8d330e
axyz1288 <axyz1350@gmail.com>
2019-11-19 22:03:03 +0800

    upload files

---
+++ README.md
@@ -0,0 +1,95 @@
+# Pytorch_Analyzer
+
+This tool for Pytorch can analyze your model. 
+
+Information include **layer memory usage, max memory usage, cumulative memory usage and execution time.**
+
+# Requirement
+
+>pytorch 
+> 
+>matplotlib (python -m pip install -U matplotlib)
+
+# Analysis
+##  Print the analysis.
+
+    No.    Layer_memory    Max_memory        Memory      Exec_time    Layer
+    Initial---------------------------------------------------------------------------------------------------
+    0          61.18 MB     118.39 MB      61.18 MB     3971.82 us    Input, label etc.                  
+    Forward---------------------------------------------------------------------------------------------------
+    1        6400.00 kB      87.13 MB      67.43 MB       97.75 us    Conv2d(3, 32, kernel_size=(3,      
+    2        6400.00 kB      73.68 MB      73.68 MB       29.56 us    LeakyReLU(negative_slope=0.01)     
+    3        6400.00 kB      79.93 MB      79.93 MB       52.21 us    BatchNorm2d(32, eps=1e-05, mom     
+    4        4800.00 kB      84.62 MB      84.62 MB       34.57 us    MaxPool2d(kernel_size=2, strid     
+    5        3200.00 kB     101.52 MB      87.74 MB       77.01 us    Conv2d(32, 64, kernel_size=(3,     
+    6        3200.00 kB      90.87 MB      90.87 MB       25.99 us    LeakyReLU(negative_slope=0.01)     
+    7        3200.00 kB      93.99 MB      93.99 MB       44.82 us    BatchNorm2d(64, eps=1e-05, mom     
+    8        2400.00 kB      96.34 MB      96.34 MB       30.04 us    MaxPool2d(kernel_size=2, strid     
+    9        1600.00 kB     123.90 MB      97.90 MB       87.50 us    Conv2d(64, 128, kernel_size=(3     
+    10       1600.00 kB      99.46 MB      99.46 MB       25.27 us    LeakyReLU(negative_slope=0.01)     
+    11       1600.00 kB     101.02 MB     101.02 MB       43.15 us    BatchNorm2d(128, eps=1e-05, mo     
+    12        800.00 kB     101.81 MB     101.81 MB       67.95 us    Conv2d(128, 64, kernel_size=(1     
+    13        800.00 kB     102.59 MB     102.59 MB       25.27 us    LeakyReLU(negative_slope=0.01)     
+    14        800.00 kB     103.37 MB     103.37 MB       42.92 us    BatchNorm2d(64, eps=1e-05, mom     
+    15       1600.00 kB     130.93 MB     104.93 MB       75.82 us    Conv2d(64, 128, kernel_size=(3     
+    16       1600.00 kB     106.49 MB     106.49 MB       25.03 us    LeakyReLU(negative_slope=0.01)     
+    17       1600.00 kB     108.06 MB     108.06 MB       42.20 us    BatchNorm2d(128, eps=1e-05, mo     
+    18       1200.00 kB     109.23 MB     109.23 MB       29.80 us    MaxPool2d(kernel_size=2, strid     
+    19       3125.00 kB     112.28 MB     112.28 MB       72.24 us    Conv2d(128, 1000, kernel_size=     
+    20       3125.00 kB     115.33 MB     115.33 MB       24.80 us    LeakyReLU(negative_slope=0.01)     
+    21       3125.00 kB     118.38 MB     118.38 MB       43.39 us    BatchNorm2d(1000, eps=1e-05, m     
+    22          0.00 kB     118.38 MB     118.38 MB       18.60 us    Flatten()                          
+    23          2.00 kB     118.39 MB     118.39 MB       66.52 us    Linear(in_features=16000, out_     
+    24          2.00 kB     118.39 MB     118.39 MB       25.27 us    LeakyReLU(negative_slope=0.01)     
+
+## Plot the result
+
+* Every layer memory usage
+
+![png](layer.png)
+
+* Total memory usage (cumulative memory usage)
+
+![png](cumulative.png)
+
+* Every layer execution time
+
+![png](exec_time.png)
+
+# How to use
+
+1. Import Pytorch_Analyzer in your code
+
+``` python
+from pytorch_analyzer import Pytorch_Analyzer
+```
+
+2. Construct Pytorch_Analyzer and input your model.
+
+``` python
+analyzer = Pytorch_Analyzer(Your_model)
+```
+
+3. Before training/inference, please **initialize analyzer.**
+
+``` python
+analyzer.initial()
+outputs = Your_model(inputs)
+```
+
+4. Print the analysis.
+
+``` python
+analyzer.analysis()
+```
+
+5. Plot the analysis
+
+``` python
+analyzer.analysis_plot()
+```
+
+# Reference
+
+<https://pytorch.org/docs/stable/cuda.html> \
+<https://pytorch.org/tutorials/beginner/former_torchies/nnft_tutorial.html>
---
+++ pytorch_analyzer.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+from torch import cuda
+import time
+import matplotlib.pyplot as plt
+
+class Pytorch_Analyzer(object):
+    def __init__(self, _net):
+        # set parameters
+        self._count = 0
+        self._num_layer = 0
+        self._iter = 10
+        self._timestamp = time.time()
+        self._net = _net
+
+        # hooks
+        self._hooks = []
+        # load hooks
+        self.net(_net)
+        # info forward lists
+        self.layers = []
+        self.layer_memory = []
+        self.memory = []
+        self.max_memory = []
+        self.exec_time = []
+        
+    
+    def net(self, _net):
+        for module in _net.children():
+            for layer in module.children():
+                self._hooks.append(layer.register_forward_hook(self.layer))
+                self._num_layer += 1
+    
+    def layer(self, _module, _input, _output):
+        # use iter_th as our memory standard
+        if(self._count >= self._num_layer * self._iter and 
+            self._count < self._num_layer * (self._iter+1)
+        ):
+            self.exec_time.append((time.time() - self._timestamp) * 1000000)
+            self.layers.append(_module.__str__()[:30])
+            self.memory.append(abs(cuda.memory_allocated() / 1024. / 1024.))
+            self.max_memory.append(abs(cuda.max_memory_allocated() / 1024. / 1024.))
+            # remove forward_hook to accelerate net
+            if(self._count == self._num_layer * (self._iter+1)):
+                for hook in self._hooks:
+                    hook.remove()
+        # update timestamp
+        self._count += 1
+
+        # reset max memory allocated
+        cuda.reset_max_memory_allocated()
+        self._timestamp = time.time()
+
+    def initial(self):
+        if(self._count == self._num_layer * self._iter):
+            self.exec_time.append((time.time() - self._timestamp) * 1000000)
+            self.memory.append(abs(cuda.memory_allocated() / 1024. / 1024.))
+            self.max_memory.append(abs(cuda.max_memory_allocated() / 1024. / 1024.))
+            self.layers.append('Input, label etc.')
+
+        # reset max memory allocated
+        cuda.reset_max_memory_allocated()
+        self._timestamp = time.time()
+    
+    def analysis(self):
+        print('{:<3}  {:>14}  {:>12}  {:>12}  {:>13}    {:<5s}'
+        .format(
+            'No.',
+            'Layer_memory',
+            'Max_memory',
+            'Memory',
+            'Exec_time',
+            'Layer'
+            )
+        )
+        if(self.memory):
+            print('Initial---------------------------------------------------------------------------------------------------')
+            print('{:<3}  {:>11.2f} MB  {:>9.2f} MB  {:>9.2f} MB  {:>10.2f} us    {:<35s}'
+                .format(
+                    0,
+                    self.memory[0],
+                    self.max_memory[0],
+                    self.memory[0],
+                    self.exec_time[0],
+                    self.layers[0]
+                    )
+                )
+            print('Forward---------------------------------------------------------------------------------------------------')
+            for i in range(1, self._num_layer+1): 
+                print('{:<3}  {:>11.2f} kB  {:>9.2f} MB  {:>9.2f} MB  {:>10.2f} us    {:<35s}'
+                .format(
+                    i,
+                    (self.memory[i] - self.memory[i-1])*1024,
+                    self.max_memory[i],
+                    self.memory[i],
+                    self.exec_time[i],
+                    self.layers[i]
+                    )
+                )
+        else:
+            print('----------------------------------------------------------------------------------------No layers\n')
+
+    def analysis_plot(self):
+        # plot layer memory
+        plt.figure(1)
+        self.layer_memory = [(self.memory[i] - self.memory[i-1]) * 1024 for i in range(1, self._num_layer+1)]
+        plt.plot(self.layer_memory, label='layer_memory')
+        plt.title('Layer Memory Usage')
+        plt.xlabel('nth layer')
+        plt.ylabel('kB')
+        plt.legend()
+
+        # plot cumulative memory
+        plt.figure(2)
+        memory = self.memory[1:]
+        max_memory =  self.max_memory[1::]
+        plt.plot(memory, label='memory')
+        plt.plot(max_memory, label='max_memory')
+        plt.title('Cumulative Memory Usage')
+        plt.xlabel('nth layer')
+        plt.ylabel('MiB')
+        plt.legend()
+
+        # plot execution time
+        plt.figure(3)
+        exec_time = self.exec_time[1::]
+        plt.plot(exec_time, label='exec_time')
+        plt.title('Excution time')
+        plt.xlabel('nth layer')
+        plt.ylabel('us')
+        plt.legend()
+
+        plt.show()
